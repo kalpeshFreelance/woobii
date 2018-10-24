72e627290dbe1b7a911f5a26361a8c2de70e5ea2
kathanekalpesh <kathanekalpesh1990@gmail.com>
2018-10-24 23:55:50 +0530

    FIrst Code UI
    FIrst Code UI

---
+++ src/router.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home1 from './components/page/Home.vue'
+import Home from './views/Home.vue'
+import NewsRoom from './components/page/NewsRoom'
+import MediaPartner from './components/page/MediaPartner'
+import NewsRoomDetail from './components/page/NewsRoomDetail'
+import Solution from './components/page/Solution'
+import KitrchenFinder from './components/page/KirchenFinder'
+import KitrchenFinderLocation from './components/page/KirchenFinderLocation'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/newsroom',
+      name: 'newsroom',
+      component: NewsRoom
+    /*  children: [
+        {
+           path: '/mediapartnerdetail',
+           name: 'mediapartnerdetail',
+           component: MediaPartnerDetail
+        }
+      ]*/
+    },
+    {
+      path: '/solution',
+      name: 'solution',
+      component: Solution
+    },
+    {
+      path: '/mediapartner',
+      name: 'mediapartner',
+      component: MediaPartner
+    },
+    {
+      path: '/kitrchenfinder',
+      name: 'kitrchenfinder',
+      component: KitrchenFinderLocation
+    /*  children: [
+        {
+           path: '/kitrchenFinderdetail',
+           name: 'kitrchenFinderdetail',
+           component: KitrchenFinder
+        }
+      ] */
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
