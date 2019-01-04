f073131c26c060ad2cf82d3c1bf9ff8e5c33c1e2
kathanekalpesh <kathanekalpesh1990@gmail.com>
2019-01-04 11:22:23 +0530

    pagination and multiple location added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
+    primary: '#fa6e2f',
     secondary: '#424242',
     accent: '#82B1FF',
     error: '#FF5252',
